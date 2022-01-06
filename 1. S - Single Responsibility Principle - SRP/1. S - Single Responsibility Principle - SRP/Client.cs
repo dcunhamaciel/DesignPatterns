132be0b4178a3a7002892a381d7782e143a1a09d
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-06 20:41:21 -0300

    Versão SEM Solid

---
+++ 1. S - Single Responsibility Principle - SRP/1. S - Single Responsibility Principle - SRP/Client.cs
@@ -0,0 +1,27 @@
+﻿class Client
+{
+    public void CreateClient()
+    {
+
+    }
+
+    public void ReadClient()
+    {
+
+    }
+
+    public void UpdateClient()
+    {
+
+    }
+
+    public void DeleteClient()
+    {
+
+    }
+
+    public void Notify()
+    {
+        Console.WriteLine("Notificando o cliente.");
+    }
+}
