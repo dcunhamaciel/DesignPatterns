3d4d0f71b6b304cd4a02e1c4b25d891de7b1b609
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-07 11:14:53 -0300

    Versão SEM Solid

---
+++ 04. I - Interface Segregation Principle - ISP/04. I - Interface Segregation Principle - ISP/IVehicle.cs
@@ -0,0 +1,8 @@
+﻿interface IVehicle
+{
+    void ConfigureCar(string color, int year, double engine, int seats, int doors);
+
+    void ConfigureMotorcycle(string color, int year, double engine);
+
+    void StartVehicle();
+}
---
+++ 04. I - Interface Segregation Principle - ISP/04. I - Interface Segregation Principle - ISP/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car("Azul", 2022, 4.0, 2, 2);
+        Motorcycle motorcycle = new Motorcycle("Branca", 2023, 600);
+
+        Console.ReadLine();
+    }
+}
